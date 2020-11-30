ddd836c0cd5e4c20bbea37ab225a7fa9425aea2b
rileyycoreyy <65985132+rileyycoreyy@users.noreply.github.com>
2020-11-30 12:33:55 -0500

    Update fat32.c
    Forgot to add new struct and functions

--- fat32.c
+++ fat32.c
@@ -10,7 +10,6 @@
 // https://man7.org/linux/man-pages/man2/open.2.html
 // https://www.man7.org/linux/man-pages/man2/lseek.2.html
 // https://man7.org/linux/man-pages/man2/read.2.html
-// https://linux.die.net/man/1/hexedit
 //**************************** Useful Data Types *****************************
 //unsigned char  (8 bits w/o sign)
 //unsigned short (16 bits w/o sign)
@@ -20,93 +19,129 @@
 // File and dir names are within working directory, NO PATHS.
 // STRING is always contained within "".
 // ***************************************************************************
-
-//FAT region begins at 0x4000 (byte number 16384)
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <fcntl.h>
 #include <unistd.h>
-                                     //Token struct/funcs from proj 1.
+
 typedef struct {
         int size;
         char **items;
 } tokenlist;
+
+//taken from FAT32_Spec Page 23-24
+struct  DIRENTRY
+{
+        unsigned char DIR_Name[11];
+        unsigned char DIR_Attr;
+        unsigned char DIR_NTRes;
+        unsigned char DIR_CrtTimeTenth;
+        unsigned short DIR_CrtTime;
+        unsigned short DIR_CrtDate;
+        unsigned short DIR_LstAccDate;
+        unsigned short DIR_FstClusHI;
+        unsigned short DIR_WrtTime;
+        unsigned short DIR_WrtDate;
+        unsigned short DIR_FstClusLO;
+        unsigned int DIR_FileSize;
+} __attribute__((packed));
 
 tokenlist *get_tokens(char *input);
 tokenlist *new_tokenlist(void);
 char *get_input(void);
 void add_token(tokenlist *tokens, char *item);
 void free_tokens(tokenlist *tokens);
-									 //Proj 3 helper functions!
-unsigned int GetFATOffset(unsigned int N);
+
+unsigned int GetByteOffset(unsigned int N);
 unsigned int GetClustEntry(unsigned int offset);
 unsigned int GetDataOffset(unsigned int N);
 bool EndCluster(unsigned int entry);
-struct DIRENTRY * GetDirectoryEntries(unsigned int N);
+unsigned int ThisFATSecNum(unsigned int N);
+unsigned int ThisFATEntOffset(unsigned int N);
+unsigned int nextCluster(unsigned int N);
 
-unsigned char sectPerClust, numFats;
-unsigned short bytePerSect, resSectCount;
-unsigned int totalSects, FATsize, rootClust;
+        unsigned char sectPerClust, numFats;
+        unsigned short bytePerSect, resSectCount;
+        unsigned int totalSects, FATsize, rootClust;
+        int filedesc;
+        tokenlist *tokens;
 
 int main()
-{                                     //Metadata for the boot sector.
-    int filedesc = open("fat32.img", O_RDONLY);
-	lseek(filedesc, 11, SEEK_SET);    //11 byte offset for BPB_bytsPerSec.
-    read(filedesc, &bytePerSect, 2);  //Read 2 bytes.					
-	lseek(filedesc, 13, SEEK_SET);    //13 byte offset for BPB_bytsPerSec.
-    read(filedesc, &sectPerClust, 1); //read 1 byte for bpb_bytspersec.					
-	lseek(filedesc, 14, SEEK_SET);    //Same pattern for remaining metadata...
-    read(filedesc, &resSectCount, 2); //BPB_RsvdSecCnt.
-	lseek(filedesc, 16, SEEK_SET);    //BPB_NumFATs.
-    read(filedesc, &numFats, 1);
-	lseek(filedesc, 32, SEEK_SET);    //BPB_TotSec32.
-    read(filedesc, &totalSects, 4);		
-	lseek(filedesc, 36, SEEK_SET);    //BPB_FATSz32.
-    read(filedesc, &FATsize, 4);	
-	lseek(filedesc, 44, SEEK_SET);    //BPB_RootClus.
-    read(filedesc, &rootClust, 4);
-    close(filedesc);
-	
-	unsigned int byteOffset = GetDataOffset(rootClust) * 512;
-	printf("Temporary... Start of data region (Decimal): %u\n", byteOffset);
-	
-	while (1)						  //Begin main loop for user input.
-	{
-		printf("$ ");
-		char *input = get_input();	  //Tokenize user input.
-		tokenlist *tokens = get_tokens(input);
-		//printf("Token is: %s\n", tokens->items[0]);
-		
-		if(strcmp(tokens->items[0], "quit") == 0)
-			break;                    //Exit program on "quit".
+{
+        filedesc = open("fat32.img", O_RDONLY);
+        lseek(filedesc, 11, SEEK_SET);    //11 byte offset for BPB_bytsPerSec.
+        read(filedesc, &bytePerSect, 2);  //Read 2 bytes.
 
-		if(strcmp(tokens->items[0], "info") == 0)
-		{                             //Print metadata collected at start.
-			printf("bytePerSect: %u\n", bytePerSect);
-			printf("sectPerClust: %u\n", sectPerClust);
-			printf("resSectCount: %u\n", resSectCount);
-			printf("numFats: %u\n", numFats);
-			printf("totalSects: %u\n", totalSects);
-			printf("FATsize: %u\n", FATsize);
-			printf("rootClust: %u\n", rootClust);
-		}
-		else if(strcmp(tokens->items[0], "size") == 0)
-		{
-			if(tokens->size == 2)	//If FILENAME was provided...
-			{
-				//Print size of FILENAME in curr directory (bytes).
-			}
-			else					//Else, print error...
-			{
-				//Print error.
-			}
-		}
-		else if(strcmp(tokens->items[0], "ls") == 0)
-		{							// If a DIRNAME was given...
-			//The first sector of the beginning of the data region (cluster #2) is computed as given below:
+        lseek(filedesc, 13, SEEK_SET);    //13 byte offset for BPB_bytsPerSec.
+        read(filedesc, &sectPerClust, 1); //read 1 byte for bpb_bytspersec.
+
+        lseek(filedesc, 14, SEEK_SET);    //Same pattern for remaining metadata...
+        read(filedesc, &resSectCount, 2); //BPB_RsvdSecCnt.
+
+        lseek(filedesc, 16, SEEK_SET);    //BPB_NumFATs.
+        read(filedesc, &numFats, 1);
+
+        lseek(filedesc, 32, SEEK_SET);    //BPB_TotSec32.
+        read(filedesc, &totalSects, 4);
+
+        lseek(filedesc, 36, SEEK_SET);    //BPB_FATSz32.
+        read(filedesc, &FATsize, 4);
+
+        lseek(filedesc, 44, SEEK_SET);    //BPB_RootClus.
+        read(filedesc, &rootClust, 4);
+        //close(filedesc);
+
+        while (1)                                                 //Begin main loop for user input.
+        {
+                printf("$ ");
+                char *input = get_input();        //Tokenize user input.
+                tokens = get_tokens(input);
+                //printf("Token is: %s\n", tokens->items[0]);
+
+                if(strcmp(tokens->items[0], "quit") == 0)
+                        break;                    //Exit program on "quit".
+
+                if(strcmp(tokens->items[0], "info") == 0)
+                {                             //Print metadata collected at start.
+                        printf("bytePerSect: %u\n", bytePerSect);
+                        printf("sectPerClust: %u\n", sectPerClust);
+                        printf("resSectCount: %u\n", resSectCount);
+                        printf("numFats: %u\n", numFats);
+                        printf("totalSects: %u\n", totalSects);
+                        printf("FATsize: %u\n", FATsize);
+                        printf("rootClust: %u\n", rootClust);
+                }
+                else if(strcmp(tokens->items[0], "size") == 0)
+                {
+/*                      if(tokens->size == 2)   //If FILENAME was provided...
+                        {
+                                int flag = 0;
+                                DIRENTRY* temp = GetDirectoryEntries(CURRENTCLUSTERNUM); //must be changed to currentcluster when we have workings for it
+                                for (int i = 0; i < sizeof(temp); i++)
+                                {
+                                        if (strncmp(temp[i].DIR_Name, tokens->items[1], sizeof(temp[i].DIR_NAME)) == 0)
+                                        {
+                                                printf("the file is %u bytes\n", temp[i].DIR_FileSize);
+                                                flag = 1;	//file found
+                                                break;
+                                        }
+                                }
+                                if (flag == 0)
+                                        printf("Error: file does not exist\n");
+                        }
+                        else                                    //Else, print error...
+                        {
+                                //Print error.
+                                printf("Error: No filename provided\n");
+                        }
+*/
+                }
+
+                else if(strcmp(tokens->items[0], "ls") == 0)
+                {
+                        //The first sector of the beginning of the data region (cluster #2) is computed as given below:
                         //FirstDataSector = BPB_ResvdSecCnt + (BPB_NumFATs * FATSz)  + RootDirSectors                                   // If a DIRNAME was given...
                         unsigned short firstdata = resSectCount + (numFats * FATsize);
                         unsigned long bytesperclust = bytePerSect * sectPerClust;
@@ -114,6 +149,9 @@ int main()
                         struct DIRENTRY mydirentry;
                         if(tokens->size == 2)
                         {
+                                printf("SEARCHING....\n");
+                                printf("\n");
+
                                 lseek(filedesc, firstdata * bytesperclust, SEEK_SET);   //started from root cluster but this needs to be whatever the current cluster is
                                 char mystring[] = "";
                                 while (1)
@@ -131,7 +169,7 @@ int main()
                                         else
                                             	break;  //we found the match!
                                 }
-
+                                printf("\n");
                                 //if they match, that is the structure we are looking for
                                 //combine DIR_FstClusHI and DIR_FstClusLO
                                 printf("high: %x\n", mydirentry.DIR_FstClusHI);
@@ -154,7 +192,7 @@ int main()
 
                                 //get offset for DIRNAME's first cluster and read entries
                                         printf("offset is: %d\n", GetDataOffset(x));                    //go to sector of hi+lo
-                                        lseek(filedesc, GetDataOffset(x), SEEK_SET);			//IS THIS MY PROBLEM? 
+                                        lseek(filedesc, GetDataOffset(x), SEEK_SET);                    //IS THIS MY PROBLEM?
                                         char myname[] = "";                                             //new holder for names
                                 while (1)
                                 {
@@ -177,21 +215,21 @@ int main()
                                 }
                                 //if you reach end of cluster, get next cluster from FAT and continue reading if any
                                 //we need another loop
-			}
-			else					//Else, just execute "ls".
-			{
-				
-			}
-		}
-		//...
-		//...
-		//...
-		
-		free(input);
-		free_tokens(tokens);
-	}
+                        }//end if
+                        else                                    //Else, just execute "ls".
+                        {
+                        }//end else
+                }//end ls
+                //...
+                //...
+                //...
+
+                free(input);
+                free_tokens(tokens);
+        }
 }
-struct DIRENTRY * GetDirectoryEntries(unsigned int N)
+
+struct DIRENTRY GetDirectoryEntries(unsigned int N)
 {
         struct DIRENTRY mydirentry;
         unsigned int holdclustnum = N;
@@ -231,47 +269,55 @@ struct DIRENTRY * GetDirectoryEntries(unsigned int N)
         return mydirentry;
 }
 
-unsigned int GetFATOffset(unsigned int N)      //N is cluster N.
+unsigned int nextCluster(unsigned int N)
+{
+        unsigned int next;
+        long offset;
+        offset = resSectCount*bytePerSect + N*4;
+        lseek(filedesc, offset, SEEK_SET);
+        read(filedesc, &next, 32);
+        return next;
+}
+
+unsigned int ThisFATSecNum(unsigned int N)
+{
+        return (resSectCount + (N * 4 / bytePerSect));
+}
+unsigned int ThisFATEntOffset(unsigned int N)
+{
+        return ((N * 4) % bytePerSect);
+}
+unsigned int GetByteOffset(unsigned int N)	//N is cluster N.
 {                                               //FirstFATSect = ressectcount.
-	return (resSectCount * bytePerSect + (N * 4));
+        return (resSectCount * bytePerSect + (N * 4));
 }
 
 unsigned int GetClustEntry(unsigned int offset)
 {
-	unsigned int clustEntry;
-	
-	int file = open("fat32.img", O_RDONLY);
-	lseek(file, offset, SEEK_CUR);	//Changed from SET to CUR (watch closely)!
+        unsigned int clustEntry;
+
+        int file = open("fat32.img", O_RDONLY);
+        lseek(file, offset, SEEK_SET);
     read(file, &clustEntry, 4);
-	close(file);
-	
-	return clustEntry;
+        close(file);
+
+        return clustEntry;
 }
 
 unsigned int GetDataOffset(unsigned int N)
-{											//RETURNS SECTOR NUMBER.
-	unsigned int firstDataSect = resSectCount + (numFats * FATsize);
-	return (firstDataSect + ((N - 2) * sectPerClust));
+{
+        unsigned int firstDataSect = resSectCount + (numFats * FATsize);
+        return (firstDataSect + ((N - 2) * sectPerClust));
 }
+
 
 bool EndCluster(unsigned int entry)
 {
-	if((entry <= 268435448 && entry >= 268435454) || entry == 4294967295)
-		return true;
-	else
-		return false;
+        if((entry <= 268435448 && entry >= 268435454) || entry == 4294967295)
+                return true;
+        else
+            	return false;
 }
-
-// void AccessFileContents()
-// {
-	// unsigned int offset = GetDataOffset(rootClust) * bytePerSect;
-	// while(!EndCluster(GetClustEntry(offset))
-	// {
-	//	offset = newvalue  (((update offset)))
-	// }	// 2 similar functions/loops? FATOfsset vs DataOffset????
-
-// }
-
 
 // THE FOLLOWING 5 HELPER FUNCTIONS ARE TAKEN FROM PROJECT 1 TO PROCESS INPUT.
 // ---------------------------------------------------------------------------
@@ -286,66 +332,67 @@ tokenlist *new_tokenlist(void)
 
 void add_token(tokenlist *tokens, char *item)
 {
-	int i = tokens->size;
+        int i = tokens->size;
 
-	tokens->items = (char **) realloc(tokens->items, (i + 2) * sizeof(char *));
-	tokens->items[i] = (char *) malloc(strlen(item) + 1);
-	tokens->items[i + 1] = NULL;
-	strcpy(tokens->items[i], item);
+        tokens->items = (char **) realloc(tokens->items, (i + 2) * sizeof(char *));
+        tokens->items[i] = (char *) malloc(strlen(item) + 1);
+        tokens->items[i + 1] = NULL;
+        strcpy(tokens->items[i], item);
 
-	tokens->size += 1;
+        tokens->size += 1;
 }
 
 char *get_input(void)
 {
-	char *buffer = NULL;
-	int bufsize = 0;
+        char *buffer = NULL;
+        int bufsize = 0;
 
-	char line[5];
-	while (fgets(line, 5, stdin) != NULL)
-		{
-		int addby = 0;
-		char *newln = strchr(line, '\n');
-		if (newln != NULL)
-				addby = newln - line;
-		else
-				addby = 5 - 1;
+        char line[5];
+        while (fgets(line, 5, stdin) != NULL)
+                {
+                int addby = 0;
+                char *newln = strchr(line, '\n');
+                if (newln != NULL)
+                                addby = newln - line;
+                else
+                                addby = 5 - 1;
 
-		buffer = (char *) realloc(buffer, bufsize + addby);
-		memcpy(&buffer[bufsize], line, addby);
-		bufsize += addby;
+                buffer = (char *) realloc(buffer, bufsize + addby);
+                memcpy(&buffer[bufsize], line, addby);
+                bufsize += addby;
 
-		if (newln != NULL)
-				break;
-	}
+                if (newln != NULL)
+                                break;
+        }
 
 	buffer = (char *) realloc(buffer, bufsize + 1);
-	buffer[bufsize] = 0;
+        buffer[bufsize] = 0;
 
-	return buffer;
+        return buffer;
 }
 
 tokenlist *get_tokens(char *input)
 {
-	char *buf = (char *) malloc(strlen(input) + 1);
-	strcpy(buf, input);
+        char *buf = (char *) malloc(strlen(input) + 1);
+        strcpy(buf, input);
 
-	tokenlist *tokens = new_tokenlist();
+        tokenlist *tokens = new_tokenlist();
 
-	char *tok = strtok(buf, " ");
-	while (tok != NULL) {
-			add_token(tokens, tok);
-			tok = strtok(NULL, " ");
-	}
+        char *tok = strtok(buf, " ");
+        while (tok != NULL) {
+                        add_token(tokens, tok);
+                        tok = strtok(NULL, " ");
+        }
 
 	free(buf);
-	return tokens;
+        return tokens;
 }
 
 void free_tokens(tokenlist *tokens)
 {
-	for (int i = 0; i < tokens->size; i++)
-			free(tokens->items[i]);
+        for (int i = 0; i < tokens->size; i++)
+                        free(tokens->items[i]);
 
-	free(tokens);
+        free(tokens);
 }
+
